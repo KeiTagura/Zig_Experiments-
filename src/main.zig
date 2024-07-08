8651bd7686a5aaca2f1a33e2ca73f0f069bf5518
KeiTagura <34691764+KeiTagura@users.noreply.github.com>
2024-07-08 22:06:10 +0900

    Update: var test

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,22 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+pub fn main() void {
+    var user = User{
+        .id = 1,
+        .power = 100,
+    };
+    user.power += 0;
+    // this line has been added
+    levelUp(user);
+    std.debug.print("User {d} has power of {d}\n", .{ user.id, user.power });
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn levelUp(user: User) void {
+    var u = user;
+    u.power += 1;
 }
+
+pub const User = struct {
+    id: u64,
+    power: i32,
+};
